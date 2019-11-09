d2f78afdba1f46f9545d8c52efe4221a693a4128
Lauri Gustafsson <me@gustafla.space>
2019-11-09 13:25:04 +0200

    Add Multiplier

--- gradle-tutorial/src/main/java/Main.java
+++ gradle-tutorial/src/main/java/Main.java
@@ -1,12 +1,14 @@
 import java.util.*;
+import ohtu.Multiplier;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("kuka olet: ");
-        String nimi = scanner.nextLine();
+        Multiplier kolme = new Multiplier(3);
+        System.out.println("anna luku ");
+        int luku = scanner.nextInt();
 
-        System.out.println("Hello " + nimi);
+        System.out.println("luku kertaa kolme on " + kolme.multipliedBy(luku) );
     }
 }
 
