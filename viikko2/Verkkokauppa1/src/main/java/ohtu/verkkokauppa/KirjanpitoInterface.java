26b3ff29aa596d7503a7c7014f683d16c907369c
Lauri Gustafsson <me@gustafla.space>
2019-11-11 23:38:09 +0200

    Remove singletons

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/KirjanpitoInterface.java
@@ -0,0 +1,8 @@
+package ohtu.verkkokauppa;
+
+import java.util.ArrayList;
+
+public interface KirjanpitoInterface {
+    public void lisaaTapahtuma(String tapahtuma);
+    public ArrayList<String> getTapahtumat();
+}
